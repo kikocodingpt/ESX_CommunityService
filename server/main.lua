fca5ef90da64a11907c8508760913150a601359d
kikocodingpt <95388281+kikocodingpt@users.noreply.github.com>
2023-11-13 15:42:48 +0000

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -1,6 +1,4 @@
-ESX = nil
-
-TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+ESX = exports["es_extended"]:getSharedObject()
 
 
 TriggerEvent('es:addGroupCommand', 'comserv', 'admin', function(source, args, user)
